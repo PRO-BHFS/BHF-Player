8294a0c6a67180ac7d481832f6a695980d72cd0f
Mohammed Faisal <bo2012faisal@gmail.com>
2025-09-11 05:30:30 +0300

    Add screenshot control functionality for Android and iOS

--- android/app/src/main/kotlin/com/example/bhf_player/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/bhf_player/MainActivity.kt
@@ -1,5 +1,35 @@
 package com.example.bhf_player
 
+import android.os.Bundle
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "screenshot_listener"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            when (call.method) {
+                "disableScreenshot" -> {
+                    window.setFlags(
+                        WindowManager.LayoutParams.FLAG_SECURE,
+                        WindowManager.LayoutParams.FLAG_SECURE
+                    )
+                    result.success(null)
+                }
+                "enableScreenshot" -> {
+                    window.clearFlags(WindowManager.LayoutParams.FLAG_SECURE)
+                    result.success(null)
+                }
+                else -> {
+                    result.notImplemented()
+                }
+            }
+        }
+    }
+}
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,26 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+     let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    
+    let channel = FlutterMethodChannel(
+      name: "screenshot_listener",
+      binaryMessenger: controller.binaryMessenger
+    )
+
+    channel.setMethodCallHandler { (call: FlutterMethodCall, result: @escaping FlutterResult) in
+      switch call.method {
+      case "disableScreenshot":
+        controller.view.window?.layer.superlayer?.isHidden = true
+        result(nil)
+      case "enableScreenshot":
+        controller.view.window?.layer.superlayer?.isHidden = false
+        result(nil)
+      default:
+        result(FlutterMethodNotImplemented)
+      }
+    }
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
